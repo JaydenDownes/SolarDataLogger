797ecd1ccde935e9a54340bd022f52abf280a825
Jayden Downes <35357527+JaydenDownes@users.noreply.github.com>
2023-08-17 16:24:39 +0800

    Update script.ino
    Updated calibrations from ADC

--- script.ino
+++ script.ino
@@ -1,128 +1,139 @@
-#include <SPI.h>
 #include <SD.h>
 #include <LiquidCrystal.h>
 
-// Pins Configuration
-#define START_BUTTON_PIN    0
-#define STOP_BUTTON_PIN     1
-#define NEW_FILE_BUTTON_PIN A2
-#define LDR_SENSOR_PIN      A0
-#define CURRENT_SENSOR_PIN  A1
-#define VOLTAGE_SENSOR_PIN  2
-#define TEMPERATURE_SENSOR_PIN A3
-#define SD_CS_PIN           4
-#define LCD_RS 7
-#define LCD_EN 8
-#define LCD_D4 5
-#define LCD_D5 6
-#define LCD_D6 9
-#define LCD_D7 10
+// Pin configurations
+const int VOLTAGE_PIN = A0;
+const int CURRENT_SENSOR_PIN = A1;
+const int TEMP_SENSOR_PIN = A2;
+const int LUX_SENSOR_PIN = 10; // Duinotech LDR Sensor Module
+const int SD_CARD_PIN = 4;
+const int BUTTON_START_PIN = A3;
+const int BUTTON_STOP_PIN = A4;
+const int BUTTON_NEWFILE_PIN = A5;
+const int LCD_RS = 7;
+const int LCD_EN = 6;
+const int LCD_D4 = 5;
+const int LCD_D5 = 4;
+const int LCD_D6 = 3;
+const int LCD_D7 = 2;
 
-// LCD initialization
 LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 
-// SD File
 File dataFile;
 
-// Data Logging State
-bool loggingData = false;
+bool isLogging = false;
+int fileCounter = 0;
 
 void setup() {
-  // Start Serial communication
   Serial.begin(9600);
-
-  // Set up the buttons with internal pull-up resistors
-  pinMode(START_BUTTON_PIN, INPUT_PULLUP);
-  pinMode(STOP_BUTTON_PIN, INPUT_PULLUP);
-  pinMode(NEW_FILE_BUTTON_PIN, INPUT_PULLUP);
-
-  // Initialize LCD
+  pinMode(BUTTON_START_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_STOP_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_NEWFILE_PIN, INPUT_PULLUP);
   lcd.begin(16, 2);
 
-  // Initialize SD card
-  if (!SD.begin(SD_CS_PIN)) {
-    Serial.println("SD Card Initialization Failed!");
+  if (!SD.begin(SD_CARD_PIN)) {
+    Serial.print("SD card initialization failed. Error code: ");
+    Serial.println(SD.cardErrorCode(), HEX);
+    Serial.print("Data error code: ");
+    Serial.println(SD.dataErrorCode(), HEX);
+    lcd.print("SD Init Failed!");
     return;
   }
+  lcd.print("System Ready");
 }
 
 void loop() {
   // Check for button presses
-  if (digitalRead(START_BUTTON_PIN) == LOW) {
-    loggingData = true;
-    startNewFile();
+  if (digitalRead(BUTTON_START_PIN) == LOW) {
+    startLogging();
   }
-  if (digitalRead(STOP_BUTTON_PIN) == LOW) {
-    loggingData = false;
-    dataFile.close();
+  if (digitalRead(BUTTON_STOP_PIN) == LOW) {
+    stopLogging();
   }
-  if (digitalRead(NEW_FILE_BUTTON_PIN) == LOW && loggingData) {
-    dataFile.close();
-    startNewFile();
+  if (digitalRead(BUTTON_NEWFILE_PIN) == LOW) {
+    createNewFile();
   }
 
-  // Check for console commands
+  // Check for serial commands
   if (Serial.available()) {
     char command = Serial.read();
-    if (command == 's') { loggingData = true; startNewFile(); }
-    if (command == 'x') { loggingData = false; dataFile.close(); }
-    if (command == 'n' && loggingData) { dataFile.close(); startNewFile(); }
+    switch (command) {
+      case 's':
+        startLogging();
+        break;
+      case 't':
+        stopLogging();
+        break;
+      case 'n':
+        createNewFile();
+        break;
+    }
   }
 
-  // Log data if logging is enabled
-  if (loggingData) {
+  if (isLogging) {
     logData();
   }
+}
 
-  delay(200); // Short delay for button debouncing
+void startLogging() {
+  if (!isLogging) {
+    dataFile = SD.open("data" + String(fileCounter) + ".csv", FILE_WRITE);
+    if (dataFile) {
+      dataFile.println("Voltage,Current,Temperature,Lux");
+      isLogging = true;
+      lcd.print("Logging Started");
+    } else {
+      lcd.print("File Open Failed");
+    }
+  }
+}
+
+void stopLogging() {
+  if (isLogging) {
+    dataFile.close();
+    isLogging = false;
+    lcd.print("Logging Stopped");
+  }
+}
+
+void createNewFile() {
+  if (isLogging) {
+    stopLogging();
+  }
+  fileCounter++;
+  lcd.print("New File Created");
 }
 
 void logData() {
-  // Read Sensors
-  int lux = analogRead(LDR_SENSOR_PIN);
-  int temperature = analogRead(TEMPERATURE_SENSOR_PIN); // You may need to convert this to actual temperature
-  int current = analogRead(CURRENT_SENSOR_PIN); // You may need to convert this to actual current
-  int voltage = analogRead(VOLTAGE_SENSOR_PIN); // You may need to convert this to actual voltage
+  // Read the voltage
+  float voltage = (analogRead(VOLTAGE_PIN) / 1023.0) * 5.0 * ((15.0 + 10.0) / 10.0); // 15K and 10K voltage divider
+  // Read the current using ACS712
+  float current = (analogRead(CURRENT_SENSOR_PIN) - 512) * 5.0 / 1023.0 / 0.185;
+  // Read the temperature using Ks0033 keyestudio Analog Temperature Sensor
+  double val = analogRead(TEMP_SENSOR_PIN);
+  double fenya = (val / 1023) * 5;
+  double r = (5 - fenya) / fenya * 4700;
+  float temperature = 1 / (log(r / 10000) / 3950 + 1 / (25 + 273.15)) - 273.15;
+  // Read the LUX (please add your code for LUX calculation)
+  float lux = analogRead(LUX_SENSOR_PIN); // Placeholder for LUX reading
 
-  // Print data to LCD
+  // Log to SD card
+  dataFile.print(voltage); dataFile.print(",");
+  dataFile.print(current); dataFile.print(",");
+  dataFile.print(temperature); dataFile.print(",");
+  dataFile.println(lux);
+
+  // Print to serial
+  Serial.print(voltage); Serial.print("\t");
+  Serial.print(current); Serial.print("\t");
+  Serial.print(temperature); Serial.print("\t");
+  Serial.println(lux);
+
+  // Display on LCD
   lcd.setCursor(0, 0);
-  lcd.print("L:");
-  lcd.print(lux);
-  lcd.print(" C:");
-  lcd.print(current);
+  lcd.print("V: "); lcd.print(voltage);
+  lcd.print(" C: "); lcd.print(current);
   lcd.setCursor(0, 1);
-  lcd.print("V:");
-  lcd.print(voltage);
-  lcd.print(" T:");
-  lcd.print(temperature);
-
-  // Print data to Serial console
-  Serial.print("Lux: "); Serial.print(lux);
-  Serial.print(" Current: "); Serial.print(current);
-  Serial.print(" Voltage: "); Serial.print(voltage);
-  Serial.print(" Temperature: "); Serial.println(temperature);
-
-  // Write data to the file
-  dataFile.print(lux);
-  dataFile.print(", ");
-  dataFile.print(current);
-  dataFile.print(", ");
-  dataFile.print(voltage);
-  dataFile.print(", ");
-  dataFile.println(temperature);
-}
-
-void startNewFile() {
-  char filename[] = "DATA00.CSV";
-  for (uint8_t i = 0; i < 100; i++) {
-    filename[4] = i / 10 + '0';
-    filename[5] = i % 10 + '0';
-    if (!SD.exists(filename)) {
-      break;
-    }
-  }
-  dataFile = SD.open(filename, FILE_WRITE);
-  if (dataFile) {
-    dataFile.println("Lux, Current, Voltage, Temperature");
-  }
+  lcd.print("T: "); lcd.print(temperature);
+  lcd.print(" L: "); lcd.print(lux);
 }
